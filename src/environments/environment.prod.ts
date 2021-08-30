e05cabb2e77c2f1243efe5f42277d95562c2eacb
Ajinkya Gore <ajinkyagore2@gmail.com>
2021-08-30 12:41:01 +0530

    Added api urls in env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  tradeUrl: "http://trade-service-hackathon.punedevopsa4.conygre.com",
+  userUrl: "http://user-service-hackathon.punedevopsa4.conygre.com",
+  portfolioUrl: "http://portfolio-service-hackathon.punedevopsa4.conygre.com",
+  moneyUrl: "http://money-service-hackathon.punedevopsa4.conygre.com",
+
 };
